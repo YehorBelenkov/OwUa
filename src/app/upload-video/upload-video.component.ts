e602a88ba875b1e8eeebbd618bb683047e9fe032
YehorBelenkov <yehorbelenkov@gmail.com>
2024-01-16 14:54:53 -0600

    changes

--- src/app/upload-video/upload-video.component.ts
+++ src/app/upload-video/upload-video.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import axios from 'axios';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-upload-video',
@@ -19,18 +20,34 @@ export class UploadVideoComponent {
     this.video.file = event.target.files[0];
   }
 
-  onSubmit(): void {
+  onSubmit(videoForm: NgForm): void {
+    // Handle any additional logic here
+    
     const formData = new FormData();
     formData.append('title', this.video.title);
     formData.append('description', this.video.description);
     formData.append('categoryId', "2");
     formData.append('accessStatusId', "1");
+  
     // Check if file is selected before appending
     if (this.video.file !== null) {
       formData.append('file', this.video.file);
     }
+
+    // Set up headers
+    const headers = {
+      'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyMUBtYWlsLmNvbSIsImlhdCI6MTcwNTQzNzg2MSwiZXhwIjoxNzA1NTI0MjYxfQ.-GyGkaZl6Ncm_Q-FY_5OM6sHljnPsrQy7RU4ono0j_E',
+      'Content-Type': 'multipart/form-data; boundary=<calculated when request is sent>',
+      'Content-Length': '<calculated when request is sent>',
+      'Host': '<calculated when request is sent>',
+      'User-Agent': '',
+      'Accept': '*/*',
+      'Accept-Encoding': 'gzip, deflate, br',
+      'Connection': 'keep-alive',
+      // Add other headers as needed
+    };
   
-    axios.post('http://localhost:8085/video/uploadNew', formData)
+    axios.post('http://localhost:8085/video/uploadNew', formData, { headers })
       .then(response => {
         console.log('Success', response);
         // Handle success
@@ -39,5 +56,8 @@ export class UploadVideoComponent {
         console.error('Error', error);
         // Handle error
       });
+  
+    // Prevent default form submission
+    videoForm.resetForm();
   }
 }
